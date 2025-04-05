58913d3c1e2e92d5f09e2937d50064a29416cd37
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-04-05 17:42:18 +0900

    fix: exportやaliasの外だし

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+## zsh関連
+
+### zprofile書き換え
+```
+# ログインシェルを明示的に再起動
+exec zsh -l
+```
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
+
+# set directory
+current_dir=$(pwd)
 
 # Homebrewのインストール確認
 if ! command -v brew &> /dev/null; then
@@ -6,8 +9,22 @@ if ! command -v brew &> /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# 必要なパッケージのインストール
-brew install git zsh vim tmux
+# 配備
+dotfiles_dir=~/dotfiles
+
+for file in zsh/.zshrc .exports .aliases .functions; do
+  ln -sf "$dotfiles_dir/$file" "$HOME/$file"
+done
+
+# Check for Homebrew and run brew bundle if Brewfile exists
+if type brew &>/dev/null; then
+  if [ -f "$current_dir/brew/Brewfile" ]; then
+    echo "Found Brewfile. Installing brew packages..."
+    brew bundle --file="$current_dir/brew/Brewfile"
+  fi
+else
+  echo "Homebrew not found. Please install Homebrew first."
+fi
 
 # シンボリックリンクの作成スクリプトを実行
 bash $HOME/dotfiles/link.sh
